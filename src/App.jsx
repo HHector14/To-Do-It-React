c65db8af16a85a4fa5d9bdbc79f8a87f0e4848c1
HHector14 <hernandezgarcia.hectorf@gmail.com>
2025-12-02 22:45:15 -0600

    new components added in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,26 @@
-import {  useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Task from './components/Task'
+import TasksCard from './components/TasksCard'
+import Form from './components/Form'
 import { useLocalStorage } from './hooks/useLocalStorage'
 
 function App() {
-  const [task, SetTask] = useState("")
-  const [tasks, SetTasks] = useLocalStorage("tasks", []);
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useLocalStorage("tasks", []);
 
   return (
     <>
       <div className='container'>
-        <div className='section'>
-          <h1>NEW TASK</h1>
-          <textarea wrap='wrap' placeholder='TYPE A NEW TASK' value={task} className='inputText' rows="1" onChange={e => SetTask(e.target.value)}/>
-          <br />
-          <button
-          className='primary'  
-          onClick={() => {
-            if(task.trim() == ''){
-              window.alert("Ingresa la tarea que deseas agregar")
-            }else{
-              SetTasks([...tasks, task])
-              SetTask("")
-            }
-            }}>ADD</button>
-        </div>
-        <div className='section'>
-          <h1>TO DO IT</h1>
-          {tasks.length == 0 && <p style={{fontSize: '20px', fontWeight: "bold"}}>NO TASKS YET</p>}
-          {tasks.map((item,index) => (
-            <Task
-              key={index}
-              text={item}
-              tasks={tasks}
-              SetTasks={SetTasks}
-            />
-          ))}
-        </div>
+        <Form
+          task={task}
+          setTask={setTask}
+          tasks={tasks}
+          setTasks={setTasks}
+        />
+        <TasksCard
+          tasks={tasks}
+          setTasks={setTasks}
+        />
       </div>
     </>
   )
