dcc68ec97501084f84d027b7f758f31afe3761d3
HHector14 <hernandezgarcia.hectorf@gmail.com>
2025-12-05 21:48:15 -0600

    new hook to delete and change status of each task

---
+++ src/hooks/useHandleTask.js
@@ -0,0 +1,18 @@
+export default function useHandleTask(tasks, setTasks){
+  
+  const deleteTask = (task) => {
+    const newTasks = tasks.filter(p => p[0] != task[0])
+    setTasks(newTasks)
+    localStorage.setItem("tasks", JSON.stringify(newTasks))
+  }
+
+  const changeStatusTask = (setStatus, index) => {
+    const newTasks = tasks.map((t, i) => i === index ? [t[0], !t[1]] : t)
+    setTasks(newTasks)
+    setStatus(newTasks[index][1])
+    localStorage.setItem("tasks", JSON.stringify(newTasks))
+  }
+
+  return [deleteTask, changeStatusTask]
+
+}
